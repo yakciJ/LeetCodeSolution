c3d9ec557f39c398eda3a2753073bfcd96ef2711
Đỗ Đăng Huy <sieunhan482@gmail.com>
2025-04-28 23:13:06 +0700

    28/04/2025 | 2302. Count Subarrays With Score Less Than K First time solving a hard problem!!! Oh Yeah

--- LeetCodeSolution/Program.cs
+++ LeetCodeSolution/Program.cs
@@ -4,21 +4,38 @@
 
     class Program
     {
-        // 27/04/2025 | 3392. Count Subarrays of Length Three With a Condition
+        // 28/04/2025 | 2302. Count Subarrays With Score Less Than K
         public static void Main()
         {
-            Console.WriteLine(Solution(new int[] { 1, 2, 1, 4, 1 }));
+            Console.WriteLine(Solution(new int[] { 2, 1, 4, 3, 5 }, 25));
         }
-        public static int Solution(int[] nums)
+        public static long Solution(int[] nums, long k)
         {
-            int count = 0;
-            for (int i = 1; i < nums.Length - 1; i++)
+            long count = 0;
+            long sum = 0;
+            long length = 0;
+            long overflow = 0;
+            long left = 0;
+            for (int i = 0; i < nums.Length; i++)
             {
-                if (nums[i - 1] + nums[i + 1] == (double)nums[i] / 2)
+                sum += nums[i];
+                length++;
+                if (sum * length >= k)
                 {
-                    count++;
+                    count += (length - 1) * (length - 1 + 1) / 2;
+                    count -= (overflow) * (overflow + 1) / 2;
+                    overflow = length - 1;
+                }
+                while (sum * length >= k)
+                {
+                    sum -= nums[left];
+                    left++;
+                    length--;
+                    overflow--;
                 }
             }
+            count += (length) * (length + 1) / 2;
+            count -= (overflow) * (overflow + 1) / 2;
             return count;
         }
     }
