9a9f374b9f984827d317b1cfc1727952417e2d6a
Đỗ Đăng Huy <sieunhan482@gmail.com>
2025-05-19 21:16:14 +0700

    19/05/2025 | 3024. Type of Triangle bo sort cua lib di dung cua minh thi nhanh hon

--- LeetCodeSolution/Program.cs
+++ LeetCodeSolution/Program.cs
@@ -2,19 +2,19 @@
 {
     class Program
     {
-        // 16/05/2025 | 75. Sort Colors
+        // 19/05/2025 | 3024. Type of Triangle
         public static void Main()
         {
-            //Console.WriteLine(Solution([0, 0, 1]));
+            Console.WriteLine(Solution([3, 3, 3]));
         }
 
-        public static void Solution(int[] nums)
+        public static string Solution(int[] nums)
         {
             bool sort = true;
             while (sort)
             {
                 sort = false;
-                for(int i = 0; i < nums.Length - 1; i++)
+                for (int i = 0; i < nums.Length - 1; i++)
                 {
                     if (nums[i] > nums[i + 1])
                     {
@@ -25,6 +25,15 @@
                     }
                 }
             }
+            string[] types = { "equilateral", "isosceles", "scalene", "none" };
+            //Array.Sort(nums);
+            if (nums[2] < nums[0] + nums[1])
+            {
+                if (nums[2] == nums[0]) return types[0];
+                if (nums[0] == nums[1] || nums[1] == nums[2]) return types[1];
+                return types[2];
+            }
+            return types[3];
         }
     }
 }
