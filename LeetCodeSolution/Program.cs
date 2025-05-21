55ffc5fd9d023afa6655a3459e0cf64821aa2dd0
Đỗ Đăng Huy <sieunhan482@gmail.com>
2025-05-21 15:49:26 +0700

    me, hoa ra toi uu ve space memory chu k phai time, lam ngoi nghi ca sang

--- LeetCodeSolution/Program.cs
+++ LeetCodeSolution/Program.cs
@@ -5,40 +5,29 @@
         // 21/05/2025 | 73. Set Matrix Zeroes
         public static void Main()
         {
-            Solution([[1, 1, 1], [1, 0, 1], [1, 1, 1]]);
+            Solution([[1, 2, 3, 4], [5, 0, 7, 8], [0, 10, 11, 12], [13, 14, 15, 0]]);
         }
 
         public static void Solution(int[][] matrix)
         {
-            HashSet<int> rows = new HashSet<int>();
-            HashSet<int> columns = new HashSet<int>();
+            int[] rows = new int[matrix.Length];
+            int[] columns = new int[matrix[0].Length];
             for (int i = 0; i < matrix.Length; i++)
             {
                 for (int j = 0; j < matrix[0].Length; j++)
                 {
                     if (matrix[i][j] == 0)
                     {
-                        if (!rows.Contains(i))
-                        {
-                            rows.Add(i);
-                        }
-                        if (!columns.Contains(j))
-                        {
-                            columns.Add(j);
-                        }
-                        columns.Add(j);
-                    }
-                    else if (rows.Contains(i) || columns.Contains(j))
-                    {
-                        matrix[i][j] = 0;
+                        rows[i] = 1;
+                        columns[j] = 1;
                     }
                 }
             }
-            for(int i = 0; i < matrix.Length; i++)
+            for (int i = 0; i < matrix.Length; i++)
             {
-                for(int j = 0; j < matrix[0].Length; j++)
+                for (int j = 0; j < matrix[0].Length; j++)
                 {
-                    if(rows.Contains(i) || columns.Contains(j))
+                    if (rows[i] == 1 || columns[j] == 1)
                     {
                         matrix[i][j] = 0;
                     }
