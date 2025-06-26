3e87ea1a9ce1d18188c8cc9716efe325d1d3adff
Đỗ Đăng Huy <sieunhan482@gmail.com>
2025-06-26 19:54:19 +0700

    26/6/2025 | 2311. Longest Binary Subsequence Less Than or Equal to K Lang nhang cai Math.Pow vl

--- LeetCodeSolution/Program.cs
+++ LeetCodeSolution/Program.cs
@@ -2,78 +2,33 @@
 {
     class Program
     {
-        // 25/6/2025 | 2040. Kth Smallest Product of Two Sorted Arrays
+        // 26/6/2025 | 2311. Longest Binary Subsequence Less Than or Equal to K
 
         public static void Main()
         {
-            Console.WriteLine(Solution([-2, -1, 0, 1, 2], [-3, -1, 2, 4, 5], 3));
+            Console.WriteLine(Solution("00101001", 1));
         }
-        public static long Solution(int[] nums1, int[] nums2, long k)
+        public static int Solution(string s, int k)
         {
-            long right = 10000000000L;
-            long left = -10000000000L;
-            while (left < right)
+            string res = "";
+            long number = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
             {
-                long mid = left + (right - left) / 2;
-                long count = 0;
-                for (int i = 0; i < nums1.Length; i++)
+                if (s[i] == '0')
                 {
-                    if (nums1[i] > 0)
+                    res = s[i] + res;
+                }
+                else if (s[i] == '1')
+                {
+                    double tempNumber = number + Math.Pow(2, res.Length);
+                    if (tempNumber <= k)
                     {
-                        long limit = (long)Math.Floor((double)mid / nums1[i]);
-                        long c = CountLessThanOrEqual(nums2, limit);
-                        count += c;
-                    }
-                    else if (nums1[i] < 0)
-                    {
-                        long limit = (long)Math.Ceiling((double)mid / nums1[i]);
-                        long c = CountGreaterThanOrEqual(nums2, limit);
-                        count += c;
-                    }
-                    else if (nums1[i] == 0)
-                    {
-                        if (mid >= 0)
-                            count += nums2.Length;
+                        res = s[i] + res;
+                        number = (long)tempNumber;
                     }
                 }
-                if (count >= k)
-                {
-                    right = mid;
-                }
-                else left = mid + 1;
             }
-            return left;
-        }
-        public static long CountGreaterThanOrEqual(int[] nums, long limit)
-        {
-            int max = nums.Length;
-            int min = 0;
-            while (min < max)
-            {
-                int mid = (max + min) / 2;
-                if (nums[mid] < limit)
-                {
-                    min = mid + 1;
-                }
-                else max = mid;
-            }
-            int count = nums.Length - min;
-            return count;
-        }
-        public static long CountLessThanOrEqual(int[] nums, long limit)
-        {
-            int max = nums.Length;
-            int min = 0;
-            while (min < max)
-            {
-                int mid = (max + min) / 2;
-                if (nums[mid] <= limit)
-                {
-                    min = mid + 1;
-                }
-                else max = mid;
-            }
-            return min;
+            return res.Length;
         }
     }
 }
