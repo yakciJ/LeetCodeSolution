f833ba27bce41f40fc0e9ee7fc9fab6879aa6e95
Đỗ Đăng Huy <sieunhan482@gmail.com>
2025-06-28 12:31:12 +0700

    28/6/2025 | 2099. Find Subsequence of Length K With the Largest Sum Bai ez ma nhu med, khoai vcl

--- LeetCodeSolution/Program.cs
+++ LeetCodeSolution/Program.cs
@@ -2,33 +2,24 @@
 {
     class Program
     {
-        // 26/6/2025 | 2311. Longest Binary Subsequence Less Than or Equal to K
+        // 28/6/2025 | 2099. Find Subsequence of Length K With the Largest Sum
 
         public static void Main()
         {
-            Console.WriteLine(Solution("00101001", 1));
+            Console.WriteLine(Solution([3, 4, 3, 3], 2));
         }
-        public static int Solution(string s, int k)
+        public static int[] Solution(int[] nums, int k)
         {
-            string res = "";
-            long number = 0;
-            for (int i = s.Length - 1; i >= 0; i--)
+            int[] res = new int[k];
+            var paired = nums.Select((value, index) => (value, index)).ToList();
+            paired.Sort((a, b) => b.value.CompareTo(a.value));
+            int[] topKIndices = paired.Select(p => p.index).Take(k).ToArray();
+            Array.Sort(topKIndices);
+            for (int i = 0; i < k; i++)
             {
-                if (s[i] == '0')
-                {
-                    res = s[i] + res;
-                }
-                else if (s[i] == '1')
-                {
-                    double tempNumber = number + Math.Pow(2, res.Length);
-                    if (tempNumber <= k)
-                    {
-                        res = s[i] + res;
-                        number = (long)tempNumber;
-                    }
-                }
+                res[i] = nums[topKIndices[i]];
             }
-            return res.Length;
+            return res;
         }
     }
 }
