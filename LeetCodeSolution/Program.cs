671009b18e37361c56e87c8aa35621c8ad5e3f7c
Đỗ Đăng Huy <sieunhan482@gmail.com>
2025-06-30 11:20:06 +0700

    30/6/2025 | 594. Longest Harmonious Subsequence overthinking de roi tu lam kho minh

--- LeetCodeSolution/Program.cs
+++ LeetCodeSolution/Program.cs
@@ -2,50 +2,32 @@
 {
     class Program
     {
-        // 29/6/2025 | 1498. Number of Subsequences That Satisfy the Given Sum Condition
+        // 30/6/2025 | 594. Longest Harmonious Subsequence
 
         public static void Main()
         {
-            Console.WriteLine(Solution([3, 4, 3, 3], 2));
+            Console.WriteLine(Solution([1, 2, 2, 1]));
         }
-        // Số lớn hơn target = vứt, tìm 2 số đầu số cuối, là min và max thỏa mãn tổng < target, min thì auto lấy còn max thì so với min, nếu tổng < target thì lấy, k thì vứt, số mà to hơn target cũng vứt luôn, sau đó tìm toàn bộ các số nằm giữa nó, tính tổng các dãy con vẫn phải chứa min và max, sau đó giảm max xuống 1 số, rồi lại tính tiếp.
-        public static int Solution(int[] nums, int target)
+        public static int Solution(int[] nums)
         {
-            long count = 0;
-            Array.Sort(nums);
-            int left = 0;
-            int right = nums.Length-1;
-            const long MOD = 1_000_000_007;
-
-            while (left <= right && right < nums.Length)
+            int maxLength = 0;
+            Dictionary<int, int> freq = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
             {
-                if (nums[left] + nums[right] <= target)
+                if (freq.ContainsKey(nums[i]))
                 {
-                    count = (count + PowerMod(2, right - left, MOD)) % MOD;
-                    left++;
+                    freq[nums[i]]++;
                 }
-                else
+                else freq[nums[i]] = 1;
+            }
+            foreach (var num in freq.Keys)
+            {
+                if (freq.ContainsKey(num + 1))
                 {
-                    right--;
+                    maxLength = Math.Max(maxLength, freq[num] + freq[num + 1]);
                 }
             }
-            return (int)count;
-        }
-        public static long PowerMod(long baseVal, long exp, long mod)
-        {
-            long result = 1;
-            baseVal %= mod;
-
-            while (exp > 0)
-            {
-                if ((exp & 1) == 1)  // nếu exp là số lẻ
-                    result = (result * baseVal) % mod;
-
-                baseVal = (baseVal * baseVal) % mod;
-                exp >>= 1; // chia exp cho 2
-            }
-
-            return result;
+            return maxLength;
         }
     }
 }
