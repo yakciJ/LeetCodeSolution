fc4986c7270ff74a8489b1290ab803098657ec3e
Đỗ Đăng Huy <sieunhan482@gmail.com>
2025-07-14 23:02:42 +0700

    14/07/2025 | 409. Longest Palindrome

--- LeetCodeSolution/Program.cs
+++ LeetCodeSolution/Program.cs
@@ -2,31 +2,41 @@
 {
     class Program
     {
-        // 14/07/2025 | 1290. Convert Binary Number in a Linked List to Integer
+        // 14/07/2025 | 409. Longest Palindrome
         // ez
 
         public static void Main()
         {
             //Console.WriteLine(Solution([1, 0, 1]);
         }
-        // hoc ve shift left operation, hoc ve toan tu lq den bit
-        public class ListNode
-        {
-            public int val;
-            public ListNode next;
-            public ListNode(int val = 0, ListNode next = null)
-            {
-                this.val = val;
-                this.next = next;
-            }
-        }
-        public static int Solution(ListNode head)
+        // greedy
+        public static int Solution(string s)
         {
             int res = 0;
-            while (head != null)
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            for(int i = 0; i < s.Length; i++)
             {
-                res = (res << 1) | head.val;
-                head = head.next;
+                if (dict.ContainsKey(s[i]))
+                {
+                    dict[s[i]]++;
+                }
+                else dict[s[i]] = 1;
+                if (dict[s[i]] % 2 == 0)
+                {
+                    res += 2;
+                }
+            }
+            //foreach(var val in dict.Values)
+            //{
+            //    if (val % 2 == 0)
+            //    {
+            //        res += val;
+            //    }
+            //    else res += val - 1;
+            //}
+            if(res % 2 == 0 && res < s.Length)
+            {
+                res++;
             }
             return res;
         }
