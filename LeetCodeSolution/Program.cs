3ca29b3e25053e75f884a5b36f6ab8760ae15521
Đỗ Đăng Huy <sieunhan482@gmail.com>
2025-07-18 18:22:54 +0700

    18/07/2025 | 509. Fibonacci Number de quy nhung them ti dp top down

--- LeetCodeSolution/Program.cs
+++ LeetCodeSolution/Program.cs
@@ -12,15 +12,21 @@
         //  hoc ve de quy recursion
         public static int Solution(int n)
         {
-            return Fib(n);
+            int[] memo = new int[n + 1];
+            return Fib(n, memo);
         }
-        public static int Fib(int n)
+        public static int Fib(int n, int[] memo)
         {
-            if (n > 1)
+            if (n <= 1)
             {
-                return Fib(n - 1) + Fib(n - 2);
+                return n;
             }
-            else return n;
+            if (memo[n] != 0)
+            {
+                return memo[n];
+            }
+            memo[n] = Fib(n - 1, memo) + Fib(n - 2, memo);
+            return memo[n];
         }
     }
 }
