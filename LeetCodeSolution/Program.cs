ee4cc22bc7620ed7fc117f6f4aa33b511bb29e76
Đỗ Đăng Huy <sieunhan482@gmail.com>
2025-07-20 23:59:36 +0700

    20/07/2025 | 203. Remove Linked List Elements de quy kho qua

--- LeetCodeSolution/Program.cs
+++ LeetCodeSolution/Program.cs
@@ -2,14 +2,42 @@
 {
     class Program
     {
-        // 18/07/2025 | 1233. Remove Sub-Folders from the Filesystem
-        // med 
-
+        // 20/07/2025 | 203. Remove Linked List Elements
+        // ez
         public static void Main()
         {
             Console.WriteLine(Solution(["/a", "/a/b", "/c/d", "/c/d/e", "/c/f"])); // Example input  
         }
-        //  
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+        static public ListNode RemoveElements(ListNode head, int val)
+        {
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+            Recursion(dummy, val);
+            return dummy.next;
+        }
+
+        static public void Recursion(ListNode head, int val)
+        {
+            if (head == null || head.next == null) return;
+            if (head.next.val == val)
+            {
+                head.next = head.next.next;
+                Recursion(head, val);
+            }
+            else Recursion(head.next, val);
+        }
+
+
         public static IList<string> Solution(string[] folder)
         {
             Array.Sort(folder);
