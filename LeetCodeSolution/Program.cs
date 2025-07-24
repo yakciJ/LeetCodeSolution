ce5bb7ab056ca689348fadf4f10fe3ca2a6c7164
Đỗ Đăng Huy <sieunhan482@gmail.com>
2025-07-24 23:23:52 +0700

    24/07/2025 | 3110. Score of a String luoi

--- LeetCodeSolution/Program.cs
+++ LeetCodeSolution/Program.cs
@@ -2,50 +2,20 @@
 {
     class Program
     {
-        // 23/07/2025 | 1717. Maximum Score From Removing Substrings
-        // med
+        // 24/07/2025 | 3110. Score of a String
+        // ez
         public static void Main()
         {
-            Console.WriteLine(Solution("cdbcbbaaabab", 4, 5)); // Example input  
-            Console.WriteLine(Solution("aabbaaxybbaabb", 5, 4)); // Example input  
+            Console.WriteLine(Solution("hello")); // Example input  
+            Console.WriteLine(Solution("asdasdwfcvqwescxzcergtasd")); // Example input  
         }
 
-        public static int Solution(string s, int x, int y)
+        public static int Solution(string s)
         {
-            char a, b;
             int score = 0;
-            int score1 = x > y ? x : y;
-            if (x > y)
+            for (int i = 0; i < s.Length - 1; i++)
             {
-                a = 'a';
-                b = 'b';
-            }
-            else
-            {
-                a = 'b';
-                b = 'a';
-            }
-            Stack<char> chars = new Stack<char>();
-            for (int i = 0; i < s.Length; i++)
-            {
-                if (chars.Count > 0 && s[i] == b && chars.Peek() == a)
-                {
-                    score += score1;
-                    chars.Pop();
-                }
-                else chars.Push(s[i]);
-            }
-            score1 = x < y ? x : y;
-            var remaining = new string(chars.Reverse().ToArray());
-            chars = new Stack<char>();
-            for (int i = 0; i < remaining.Length; i++)
-            {
-                if (chars.Count > 0 && remaining[i] == a && chars.Peek() == b)
-                {
-                    score += score1;
-                    chars.Pop();
-                }
-                else chars.Push(remaining[i]);
+                score += Math.Abs(s[i] - s[i + 1]);
             }
             return score;
         }
