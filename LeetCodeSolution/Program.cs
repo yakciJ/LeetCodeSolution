2f60823a29db6a2e6c898543f04f737f72eb4a91
Đỗ Đăng Huy <sieunhan482@gmail.com>
2025-08-11 00:10:32 +0700

    10/08/2025 | 2108. Find First Palindromic String in the Array

--- LeetCodeSolution/Program.cs
+++ LeetCodeSolution/Program.cs
@@ -2,19 +2,36 @@
 {
     class Program
     {
-        // 09/08/2025 | 231. Power of Two
+        // 10/08/2025 | 2108. Find First Palindromic String in the Array
         // ez
+        // 
         public static void Main()
         {
-            Console.WriteLine(Solution(1));
-            Console.WriteLine(Solution(16));
-            Console.WriteLine(Solution(3));
+            Console.WriteLine(Solution(["abc", "car", "ada", "racecar", "cool"]));
+            Console.WriteLine(Solution(["notapalindrome", "racecar"]));
+            Console.WriteLine(Solution(["po", "zsz"]));
         }
 
-        public static bool Solution(int n)
+        public static string Solution(string[] words)
         {
-            return (n > 0 && (n & (n - 1)) == 0);
-            // giải thích: mũ 2 luôn luôn chỉ có 1 bit 1, nghĩa là vd: 8 = 1000, 2 = 10, .... nên n - 1 sẽ biến toàn bộ bit sau số 1 duy nhất đó thành 1 và số 1 đó sẽ thành số 0, 7 = 0111, 1 = 1, nên n & (n-1), & là cả 2 bit = 1 thì mới = 1, nên 1000 với 0111 thì sẽ thành 0000 = 0.
+            foreach (string word in words)
+            {
+                bool isPalindrome = true;
+                int length = word.Length;
+                for (int i = 0; i < word.Length / 2 + 1; i++)
+                {
+                    if (word[i] != word[length - i - 1])
+                    {
+                        isPalindrome = false;
+                        break;
+                    }
+                }
+                if (isPalindrome)
+                {
+                    return word;
+                }
+            }
+            return "";
         }
     }
 }
