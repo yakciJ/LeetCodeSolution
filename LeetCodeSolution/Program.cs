eaec4c350cb83fd430301d5369a0e76df00eeff4
Đỗ Đăng Huy <sieunhan482@gmail.com>
2025-08-12 15:45:27 +0700

    12/08/2025 | 342. Power of Four

--- LeetCodeSolution/Program.cs
+++ LeetCodeSolution/Program.cs
@@ -2,45 +2,30 @@
 {
     class Program
     {
-        // 11/08/2025 | 1913. Maximum Product Difference Between Two Pairs
+        // 12/08/2025 | 342. Power of Four
         // ez
-        // 
+        // lam ez de danh thoi gian cho prj
         public static void Main()
         {
-            Console.WriteLine(Solution([5, 6, 2, 7, 4]));
-            Console.WriteLine(Solution([4, 2, 5, 9, 7, 4, 8]));
-            //Console.WriteLine(Solution(["po", "zsz"]));
+            Console.WriteLine(Solution(16));
+            Console.WriteLine(Solution(5));
+            Console.WriteLine(Solution(1));
         }
 
-        public static int Solution(int[] nums)
+        public static bool Solution(int n)
         {
-            int num1 = int.MaxValue;
-            int num2 = int.MaxValue;
-            int num3 = int.MinValue;
-            int num4 = int.MinValue;
-            for (int i = 0; i < nums.Length; i++)
+            if (n == 1)
             {
-                if (nums[i] > num4)
-                {
-                    num3 = num4;
-                    num4 = nums[i];
-                }
-                else if (nums[i] > num3)
-                {
-                    num3 = nums[i];
-                }
-                if (nums[i] < num2)
-                {
-                    num1 = num2;
-                    num2 = nums[i];
-                }
-                else if (nums[i] < num1)
-                {
-                    num1 = nums[i];
-                }
+                return true;
             }
-            return (num3 * +num4) - (num1 * num2);
+            double a = n;
+            while (a > 1)
+            {
+                if ((a /= 4) == 1) return true;
+            }
+            return false;
         }
+        // con 1 cach nua la giong nhu 2^, cung chi co 1 so 1 trong hệ nhị phân, nhưng thêm điều kiện nữa là số 1 đó ở vị trí chẵn, 2,4,6,8,...
     }
 }
 
