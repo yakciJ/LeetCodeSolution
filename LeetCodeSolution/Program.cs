f7f94401cd9a79a03a58138ce73c5ef69641ade0
Đỗ Đăng Huy <sieunhan482@gmail.com>
2025-08-15 16:17:29 +0700

    15/08/2025 | 342. Power of Four bai nay lam r

--- LeetCodeSolution/Program.cs
+++ LeetCodeSolution/Program.cs
@@ -2,43 +2,28 @@
 {
     class Program
     {
-        // 14/08/2025 | 2264. Largest 3-Same-Digit Number in String
+        // 15/08/2025 | 342. Power of Four
         // ez
         // lam ez de danh thoi gian cho prj
         public static void Main()
         {
-            Console.WriteLine(Solution("6777133339"));
-            Console.WriteLine(Solution("2300019"));
-            Console.WriteLine(Solution("42352338"));
+            Console.WriteLine(Solution(16));
+            Console.WriteLine(Solution(5));
+            Console.WriteLine(Solution(1));
         }
 
-        public static string Solution(string num)
+        public static bool Solution(int n)
         {
-            char present = num[0];
-            int count = 1;
-            int max = -1;
-            for (int i = 1; i < num.Length; i++)
+            if (n == 1)
             {
-                if (num[i] == present)
-                {
-                    count++;
-                }
-                else
-                {
-                    count = 1;
-                    present = num[i];
-                }
-                if (count == 3)
-                {
-                    max = Math.Max(num[i] - '0', max);
-                }
+                return true;
             }
-            string a = max.ToString();
-            if (a != "-1")
+            double a = n;
+            while (a > 1)
             {
-                return a + a + a;
+                if ((a /= 4) == 1) return true;
             }
-            return "";
+            return false;
         }
     }
 }
